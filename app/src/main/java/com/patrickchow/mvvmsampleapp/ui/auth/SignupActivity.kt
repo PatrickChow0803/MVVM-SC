3f1315496466a18b12b7d0daf96efa50f7a9fc1e
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-15 10:20:16 -0400

    Enabled Data Binding, added login layout and register layout, added packages.

--- app/src/main/java/com/patrickchow/mvvmsampleapp/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/patrickchow/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.patrickchow.mvvmsampleapp
+package com.patrickchow.mvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.patrickchow.mvvmsampleapp.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
